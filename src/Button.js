6fed50065d649ea24697a661a3aa44a5595aad76
soyeon100 <dev.dorahee100@gmail.com>
2022-09-17 18:25:18 +0900

    #5 [2021 UPDATE] CREATE REACT APP - style modular

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,9 @@
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button({text}) {
     return (
-        <button style={{
-            backgroundColor: "tomato",
-            color: "yellow"
-        }}>{text}</button>
+        <button className={styles.btn}>{text}</button>
     )
 }
 
