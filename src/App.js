d3ed99fe80915af3cb84265215ad4ab0b80ed010
soyeon100 <dev.dorahee100@gmail.com>
2022-09-17 22:32:16 +0900

    #7 [2021 UPDATE] PRACTICE MOVIE APP - Coin Tracker !

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
-import ToDoList from "./ToDoList";
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+        .then(response => response.json())
+        .then(json => {
+          setCoins(json)
+          setLoading(false);
+        });
+  }, []);
+
+  // 첼린지 : USD 를 BTC 로 전환해보기
   return (
     <div>
-        <ToDoList />
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : (
+          <select>
+              {coins.map((coin) => (
+                  <option key={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD}</option>
+              ))}
+          </select>
+      )}
     </div>
   );
 }
