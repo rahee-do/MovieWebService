c6d1deb7cd1288140d6cfdf69692352ad2fb29a2
soyeon100 <dev.dorahee100@gmail.com>
2022-09-18 16:49:02 +0900

    #7 [2021 UPDATE] PRACTICE MOVIE APP - Movie App (Part One)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  // async-await 사용
+  const getMovies = async () => {
+    // await 으로 한번 더 감싸줘서 사용.
+    const json = await (
+        await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year")
+    ).json();
+    /*
+      const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year");
+      const json = await response.json();
+    */
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-        .then(response => response.json())
-        .then(json => {
-          setCoins(json)
-          setLoading(false);
-        });
+    getMovies();
+    // 영화 별점이 8.5점 이상이면서 연도별로 정렬된 데이터 API 호출
+    /*
+        .then 보다 많이 사용되는 async-await 사용하기
+        fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
+            .then(response => response.json())
+            .then(json => {
+              setMovies(json.data.movies);
+              setLoading(false);
+            });
+     */
   }, []);
-
-  // 첼린지 : USD 를 BTC 로 전환해보기
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : (
-          <select>
-              {coins.map((coin) => (
-                  <option key={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD}</option>
-              ))}
-          </select>
+      <h1>{loading ? "" : `Movies(${movies.length})`}</h1>
+      {loading ? (
+          <h1>Loading...</h1>
+      ) : (
+          <div>
+            {movies.map((movie) => (
+                <div key={movie.id}>
+                  <img src={movie.medium_cover_image} alt={`${movie.id}_medium_cover_image`}/>
+                  <h2>{movie.title_long}</h2>
+                  <p>{movie.summary === "" ? "No summary.." : movie.summary}</p>
+                  <ul>
+                    {movie.genres.map(g => (
+                        <li key={g}>{g}</li>
+                    ))}
+                  </ul>
+                </div>
+            ))}
+          </div>
       )}
     </div>
   );
